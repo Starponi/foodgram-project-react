e9d1e5981618bbf39bcc4ecec1341a0ba7ff02f3
Никита Шаров <stigii14@gmail.com>
2022-09-20 13:34:00 +0300

    TEST

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -7,7 +7,8 @@ from rest_framework.serializers import ValidationError
 from recipes.models import (
     AmountIngredient, Favorite, Ingredient, Recipe, ShoppingCart, Tag
 )
-from users.serializers import CustomUserSerializer
+from backend.foodgram.users.serializers import CustomUserSerializer
+
 from .mixins import RecipeMixin
 
 User = get_user_model()
